5eaf02c0086d027ad9b00a88a92909c7584a0941
threatthreat <grezy8007@gmail.com>
2025-08-04 08:11:33 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,20 @@
+#!/bin/bash
+
 echo "Cloning Repo...."
-if [ -z $BRANCH ]
-then
+if [ -z "$BRANCH" ]; then
   echo "Cloning main branch...."
   git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot JishuDeveloper/Ultra-Forward-Bot
 else
   echo "Cloning $BRANCH branch...."
   git clone https://github.com/JishuDeveloper/Ultra-Forward-Bot -b $BRANCH /Ultra-Forward-Bot
 fi
+
 cd JishuDeveloper/Ultra-Forward-Bot
+
 pip3 install -U -r requirements.txt
+
+# Run Flask health check server in background
+python3 app.py &
+
 echo "Starting Bot...."
 python3 main.py
